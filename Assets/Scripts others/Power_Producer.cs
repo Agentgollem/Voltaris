3765a872455bc24a02189aaec52edbe3d1e86354
Agentgollem <nils@tillet.fr>
2026-06-08 19:15:27 +0200

    added coal generator and generator

--- Assets/Scripts others/Power_Producer.cs
+++ Assets/Scripts others/Power_Producer.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
 
-public class Power_Producer
+public abstract class EnergyProducer : MonoBehaviour
 {
-    
+    [Header("Electrical Properties")]
+    [SerializeField] protected float voltageKV = 20f;
+    [SerializeField] protected float frequencyHz = 50f;
+
+    [SerializeField] protected float Amperage = 50f;
+    [Header("State")]
+    [SerializeField] protected bool isRunning = true;
+
+    public float VoltageKV => voltageKV;
+    public float FrequencyHz => frequencyHz;
+    public bool IsRunning => isRunning;
+
+    /// <summary>
+    /// Current power output in MW.
+    /// Every producer calculates this differently.
+    /// </summary>
+    public abstract float GetPowerOutputMW();
+
+    /// <summary>
+    /// Optional current output calculation.
+    /// </summary>
+    public virtual float GetCurrentAmps()
+    {
+        float powerW = GetPowerOutputMW() * 1_000_000f;
+        float voltageV = voltageKV * 1_000f;
+
+        if (voltageV <= 0)
+            return 0;
+
+        return powerW / voltageV;
+    }
 }
--- Assets/prefabs/Ressources/scripts/CoalGenerator.cs
+++ Assets/prefabs/Ressources/scripts/CoalGenerator.cs
@@ -1,6 +1,38 @@
 using UnityEngine;
 
-public class CoalGenerator
+public class CoalGenerator : EnergyProducer
 {
-    
+    [Header("Generator")]
+    [SerializeField] private float maxPowerMW = 100f;
+
+    [Header("Fuel")]
+    [SerializeField] private float coalStored = 100f;
+    [SerializeField] private float coalConsumptionPerSecond = 1f;
+
+    private void Update()
+    {
+        if (!isRunning)
+            return;
+
+        coalStored -= coalConsumptionPerSecond * Time.deltaTime;
+
+        if (coalStored <= 0)
+        {
+            coalStored = 0;
+            isRunning = false;
+        }
+    }
+
+    public override float GetPowerOutputMW()
+    {
+        if (!isRunning)
+            return 0f;
+
+        return maxPowerMW;
+    }
+
+    public void AddCoal(float amount)
+    {
+        coalStored += amount;
+    }
 }
